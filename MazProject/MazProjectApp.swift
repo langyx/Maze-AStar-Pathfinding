eaea72f71740a0d8c6a7717c8fe863b6844926f7
Yannis Lang <jopolaz@hotmail.fr>
2021-10-31 17:59:34 +0100

    Initial Commit

---
+++ MazProject/MazProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazProjectApp.swift
+//  MazProject
+//
+//  Created by Yannis Lang on 31/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct MazProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
